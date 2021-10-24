92659fe3b6cb7cad00005d5eb88ad4326602f023
gchengyu <86chengyujin@gmail.com>
2021-10-24 16:03:09 +0800

    Update Navbar.tsx

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -62,14 +62,6 @@ const Navbar = () => {
             <FontAwesomeIcon icon={['fab', 'github']} size="lg" />
           </a>
           <a
-            href={siteConfig.contact.telegram}
-            target="_blank"
-            rel="noopener noreferrer"
-            className="hover:bg-gray-200 dark:text-white dark:hover:bg-gray-700 p-2 rounded"
-          >
-            <FontAwesomeIcon icon={['fab', 'telegram']} size="lg" />
-          </a>
-          <a
             href={siteConfig.contact.qq}
             target="_blank"
             rel="noopener noreferrer"
