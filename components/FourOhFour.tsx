44e68957f5c0308f7a385bc47cd53b746ab72189
gchengyu <chengyujin86@gmail.com>
2022-01-12 02:33:04 +0800

    Update FourOhFour.tsx

--- components/FourOhFour.tsx
+++ components/FourOhFour.tsx
@@ -3,20 +3,23 @@ import { FunctionComponent } from 'react'
 
 const FourOhFour: FunctionComponent<{ errorMsg: string }> = ({ errorMsg }) => {
   return (
-    <div className="my-20">
-      <div className="md:w-1/3 w-1/2 mx-auto">
-        <Image src={'/images/empty.png'} alt="404" width={912} height={912} />
+    <div className="my-12">
+      <div className="w-1/3 mx-auto">
+        <Image src='/images/fabulous-rip-2.png' alt="404" width={912} height={912} />
       </div>
       <div className="mt-6 text-gray-500 max-w-xl mx-auto">
-        <div className="text-2xl font-bold mb-8">
+        <div className="text-xl font-bold mb-8">
           Oops, that&apos;s a <span className="underline decoration-wavy decoration-red-500">four-oh-four</span>.
         </div>
-        <div className="font-mono border border-gray-400/20 rounded p-2 mb-4 text-sm bg-gray-50 dark:bg-gray-800">
+        <div className="text-xl font-bold mb-8">
+          Oops, 服务器出了点 <span className="underline decoration-wavy decoration-red-500">问题</span>.
+        </div>
+        <div className="font-mono border border-gray-400/20 rounded p-2 mb-4 text-xs bg-gray-50 dark:bg-gray-800">
           {errorMsg}
         </div>
-        <div>
+        <div className="text-sm">
           Press{' '}
-          <kbd className="border-opacity-20 font-mono text-sm p-1 bg-gray-100 dark:bg-gray-800 border rounded">F12</kbd>{' '}
+          <kbd className="border-opacity-20 font-mono text-xs p-1 bg-gray-100 dark:bg-gray-800 border rounded">F12</kbd>{' '}
           and open devtools for more details, or seek help at{' '}
           <a
             className="text-blue-600 hover:text-blue-700 hover:underline"
@@ -26,6 +29,7 @@ const FourOhFour: FunctionComponent<{ errorMsg: string }> = ({ errorMsg }) => {
           >
             onedrive-vercel-index discussions
           </a>
+          等技术人员考完试就修，绝对不鸽！🕊
           .
         </div>
       </div>
