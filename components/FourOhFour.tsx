0218817176242b6c37a400a8d991507ec461a8e7
gchengyu <chengyujin86@gmail.com>
2022-01-12 02:50:00 +0800

    Update FourOhFour.tsx

--- components/FourOhFour.tsx
+++ components/FourOhFour.tsx
@@ -9,28 +9,13 @@ const FourOhFour: FunctionComponent<{ errorMsg: string }> = ({ errorMsg }) => {
       </div>
       <div className="mt-6 text-gray-500 max-w-xl mx-auto">
         <div className="text-xl font-bold mb-8">
-          Oops, that&apos;s a <span className="underline decoration-wavy decoration-red-500">four-oh-four</span>.
-        </div>
-        <div className="text-xl font-bold mb-8">
-          Oops, 服务器出了点 <span className="underline decoration-wavy decoration-red-500">问题</span>.
+          Oops, 服务器出了点<span className="underline decoration-wavy decoration-red-500">问题</span>.
         </div>
         <div className="font-mono border border-gray-400/20 rounded p-2 mb-4 text-xs bg-gray-50 dark:bg-gray-800">
           {errorMsg}
         </div>
         <div className="text-sm">
-          Press{' '}
-          <kbd className="border-opacity-20 font-mono text-xs p-1 bg-gray-100 dark:bg-gray-800 border rounded">F12</kbd>{' '}
-          and open devtools for more details, or seek help at{' '}
-          <a
-            className="text-blue-600 hover:text-blue-700 hover:underline"
-            href="https://github.com/spencerwooo/onedrive-vercel-index/discussions"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            onedrive-vercel-index discussions
-          </a>
           等技术人员考完试就修，绝对不鸽！🕊
-          .
         </div>
       </div>
     </div>
