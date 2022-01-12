b64d669fa91295d7855e9200a3556ee3f8c50af5
gchengyu <chengyujin86@gmail.com>
2022-01-13 04:18:37 +0800

    Update Navbar.tsx

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -73,7 +73,17 @@ const Navbar = () => {
               <span className="text-sm font-medium hidden md:inline-block">Email</span>
             </a>
           )}
-
+          
+          {siteConfig.wechat && (
+            <button
+              className="flex items-center space-x-2 dark:text-white hover:opacity-80"
+              onClick={() => setIsOpenWechat(true)}
+            >
+              <span className="text-sm font-medium">微信公众号</span>
+              <FontAwesomeIcon icon="weixin" />
+            </button>
+          )}
+          
           {tokenPresent && (
             <button
               className="dark:text-white flex items-center p-2 space-x-2 hover:opacity-80"
@@ -83,6 +93,7 @@ const Navbar = () => {
               <FontAwesomeIcon icon="sign-out-alt" />
             </button>
           )}
+         
         </div>
       </div>
 
@@ -138,6 +149,75 @@ const Navbar = () => {
                   <button
                     className="focus:outline-none focus:ring focus:ring-blue-300 hover:bg-blue-400 inline-flex items-center justify-center px-4 py-2 mr-3 space-x-2 text-white bg-blue-500 rounded"
                     onClick={() => setIsOpen(false)}
+                  >
+                    Cancel
+                  </button>
+                  <button
+                    className="focus:outline-none focus:ring focus:ring-red-300 hover:bg-red-400 inline-flex items-center justify-center px-4 py-2 space-x-2 text-white bg-red-500 rounded"
+                    onClick={() => clearTokens()}
+                  >
+                    <FontAwesomeIcon icon={['far', 'trash-alt']} />
+                    <span>Clear all</span>
+                  </button>
+                </div>
+              </div>
+            </Transition.Child>
+          </div>
+        </Dialog>
+      </Transition>
+      
+      <Transition appear show={isOpen} as={Fragment}>
+        <Dialog as="div" className="fixed inset-0 z-10 overflow-y-auto" open={isOpen} onClose={() => setIsOpenWechat(false)}>
+          <div className="min-h-screen px-4 text-center">
+            <Transition.Child
+              as={Fragment}
+              enter="ease-out duration-100"
+              enterFrom="opacity-0"
+              enterTo="opacity-100"
+              leave="ease-in duration-50"
+              leaveFrom="opacity-100"
+              leaveTo="opacity-0"
+            >
+              <Dialog.Overlay className="bg-gray-50 dark:bg-gray-800 fixed inset-0" />
+            </Transition.Child>
+
+            {/* This element is to trick the browser into centering the modal contents. */}
+            <span className="inline-block h-screen align-middle" aria-hidden="true">
+              &#8203;
+            </span>
+            <Transition.Child
+              as={Fragment}
+              enter="ease-out duration-100"
+              enterFrom="opacity-0 scale-95"
+              enterTo="opacity-100 scale-100"
+              leave="ease-in duration-50"
+              leaveFrom="opacity-100 scale-100"
+              leaveTo="opacity-0 scale-95"
+            >
+              <div className="dark:bg-gray-900 inline-block w-full max-w-md p-6 my-8 overflow-hidden text-left align-middle transition-all transform bg-white rounded-lg">
+                <Dialog.Title className="dark:text-gray-100 text-lg font-bold text-gray-900">
+                  Clear all tokens?
+                </Dialog.Title>
+                <div className="mt-2">
+                  <p className="text-sm text-gray-500">
+                    These tokens are used to authenticate yourself into password protected folders, clearing them means
+                    that you will need to re-enter the passwords again.
+                  </p>
+                </div>
+
+                <div className="dark:text-gray-100 max-h-32 mt-4 overflow-y-scroll font-mono text-sm">
+                  {siteConfig.protectedRoutes.map((r, i) => (
+                    <div key={i} className="flex items-center space-x-1">
+                      <FontAwesomeIcon icon="key" />
+                      <span className="truncate">{r}</span>
+                    </div>
+                  ))}
+                </div>
+
+                <div className="flex items-center justify-end mt-8">
+                  <button
+                    className="focus:outline-none focus:ring focus:ring-blue-300 hover:bg-blue-400 inline-flex items-center justify-center px-4 py-2 mr-3 space-x-2 text-white bg-blue-500 rounded"
+                    onClick={() => setIsOpenWechat(false)}
                   >
                     Cancel
                   </button>
