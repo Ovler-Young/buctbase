16de35a8061f8a29ac2c5ead5b9ccd32a5ed1ed9
Ovler <86chengyujin@gmail.com>
2022-02-10 14:31:12 +0800

    Update badge.ts

--- pages/api/badge.ts
+++ pages/api/badge.ts
@@ -1,7 +1,8 @@
 import countapi from 'countapi-js'
 import provideBadge from '../../utils/provide-badge'
+import type { NextApiRequest, NextApiResponse } from 'next'
 
-export default async (req, res) => {
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   if (!req.query.id) {
     return provideBadge(res, 'error', 'No id provided', 'critical')
   }
