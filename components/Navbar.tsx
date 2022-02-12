f0d19e5edf5baf673ea8ddbc123aff5264b3fa38
Ovler <86chengyujin@gmail.com>
2022-02-12 18:26:34 +0800

    Update Navbar.tsx

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -121,7 +121,7 @@ const Navbar = () => {
 
           {siteConfig.txc && (
             <a href={siteConfig.txc} className="flex items-center space-x-2 hover:opacity-80 dark:text-white">
-              <FontAwesomeIcon icon={['far', 'comment']} />
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M256 32C114.6 32 0 125.1 0 240c0 47.6 19.9 91.2 52.9 126.3C38 405.7 7 439.1 6.5 439.5c-6.6 7-8.4 17.2-4.6 26S14.4 480 24 480c61.5 0 110-25.7 139.1-46.3C192 442.8 223.2 448 256 448c141.4 0 256-93.1 256-208S397.4 32 256 32zm0 368c-26.7 0-53.1-4.1-78.4-12.1l-22.7-7.2-19.5 13.8c-14.3 10.1-33.9 21.4-57.5 29 7.3-12.1 14.4-25.7 19.9-40.2l10.6-28.1-20.6-21.8C69.7 314.1 48 282.2 48 240c0-88.2 93.3-160 208-160s208 71.8 208 160-93.3 160-208 160z"/></svg>
               <span className="hidden text-sm font-medium md:inline-block">兔小巢</span>
             </a>
           )}
@@ -135,7 +135,6 @@ const Navbar = () => {
               <span className="text-sm font-medium hidden md:inline-block">微信公众号</span>
             </button>
           )}
-          
           {tokenPresent && (
             <button
               className="flex items-center space-x-2 p-2 hover:opacity-80 dark:text-white"
